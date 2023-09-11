f26256f5309c21dec49d8356a22a3ac4e076b61b
LingZhouChen <lzcciao@gmail.com>
2023-09-11 10:55:18 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+// alternative
+//const shortGreeting = `Welcome, ${currentUser[0]}!`;
+//const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
